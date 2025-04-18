608b8cbdad8fd684598f39b149c832f771dcfb95
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2025-04-18 18:39:33 +0200

    HeroBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroBanner from "./components/HeroBanner";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <HeroBanner />
     </>
   );
 }
---
+++ src/components/HeroBanner.tsx
@@ -0,0 +1,14 @@
+const HeroBanner = () => {
+  return (
+    <section className="px-50 flex h-[80vh] items-center justify-between">
+      <h1 className="text-white text-8xl">
+        The freedom to <br /> pay with crypto,
+        <br />
+        <span className="text-[#00FEFBCC]">later</span>.
+      </h1>
+      <img src="/images/CB.webp" alt="card flexfi" />
+    </section>
+  );
+};
+
+export default HeroBanner;
