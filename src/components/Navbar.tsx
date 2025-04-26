9d50286017a7c2e6997cb4a7cfe12d53e65c4fc6
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2025-04-26 18:31:20 +0200

    waitlist

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,42 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <nav className=" px-6 lg:px-50 py-10 flex items-center justify-between">
-      <img
-        className="h-10 lg:h-20 cursor-pointer transition-transform duration-300 hover:scale-105"
-        src="/logo/flexfi-logo.webp"
-        alt="logo flexfi"
-      />
-      <ul className="flex gap-10 text-white items-center">
-        <li className="cursor-pointer relative group">
-          <span className="transition-colors duration-300 group-hover:text-[#00FEFB]">
-            Login
-          </span>
-          <div className="absolute left-0 -bottom-1 h-0.5 bg-accent w-0 group-hover:w-full transition-all duration-300"></div>
+    <nav className="px-6 lg:px-50 py-6 flex items-center justify-between ">
+      <Link to="/">
+        <img
+          className="h-10 lg:h-16 cursor-pointer transition-transform duration-300 hover:scale-105"
+          src="/logo/flexfi-logo.webp"
+          alt="FlexFi logo"
+        />
+      </Link>
+
+      <div className="hidden md:flex items-center gap-3 text-white text-sm font-semibold">
+        <a
+          href="#customers"
+          className="hover:text-[#00FEFB] transition-colors duration-300"
+        >
+          Customers
+        </a>
+        <span className="text-[#00FEFB]">|</span>
+        <a
+          href="#merchants"
+          className="hover:text-[#00FEFB] transition-colors duration-300"
+        >
+          Merchants
+        </a>
+      </div>
+
+      <ul className="flex items-center gap-6 text-white">
+        <li className="text-sm hover:text-[#00FEFB] transition-colors duration-300 cursor-pointer">
+          About Us
         </li>
-        <li className="rounded-2xl border border-[#00FEFB] px-4 py-2 cursor-pointer hover:bg-[#00FEFB] hover:text-black transition duration-300">
+        <Link
+          to="/waitlist"
+          className="rounded-2xl border border-[#00FEFB] px-4 py-2 cursor-pointer hover:bg-[#00FEFB] hover:text-black transition duration-300 text-sm"
+        >
           Join Waitlist
-        </li>
+        </Link>
       </ul>
     </nav>
   );
